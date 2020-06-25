22cb1a64a36f312a59ad6954cbf004f607ee9ef5
KhalidAlsuwayan <63962193+Khalidalsuwayan@users.noreply.github.com>
2020-06-25 11:41:17 +0300

    Delete Reading_URL.py

--- Reading_URL.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-with urllib.request.urlopen('https://s-m.com.sa/tr/d.html') as response:
-    url = response.read()
-print(url)
-file = open("text.txt", "w")
-file.write(str(url))
-file.close()
